71eabfa91cd634a445f1d766231e2a6ecdfe6db0
roarc-dev <roarc.official@gmail.com>
2025-09-16 14:44:26 +0900

    chore: remove legacy R2 endpoints; keep single api/r2 dispatcher

--- api/r2-presign.js
+++
@@ -1,6 +0,0 @@
-// Thin wrapper delegating to unified /api/r2 handler
-module.exports = async (req, res) => {
-  const handler = require('./r2')
-  req.body = { ...(req.body || {}), action: 'presign' }
-  return handler(req, res)
-}
--- api/r2-test.js
+++
@@ -1,6 +0,0 @@
-// Thin wrapper delegating to unified /api/r2 handler
-module.exports = async (req, res) => {
-  const handler = require('./r2')
-  req.query = { ...(req.query || {}), action: 'test' }
-  return handler(req, res)
-}
